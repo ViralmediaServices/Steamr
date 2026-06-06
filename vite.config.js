ead50bcb4bb874b190893d8f537f4a13ae0ddf09
ViralmediaServices <viralmediaservices@hotmail.com>
2026-06-06 15:13:32 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: "dist" },
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 1600,
+  },
   server: {
     port: 3000,
     proxy: { "/api": { target: "http://localhost:3001", changeOrigin: true } },
